2933a97d79c96b126cb3c0f91b377f40d8e82f5c
larsvers <larsverspohl@gmail.com>
2020-06-03 16:18:34 +0100

    Add and load data

--- src/app/init.js
+++ src/app/init.js
@@ -6,12 +6,18 @@ import {
   MeshStandardMaterial,
 } from 'three/build/three.module';
 
+import { csv } from 'd3-fetch/src/index';
 import scene from '../core/scene';
 import camera from '../core/camera';
 import { ambientLight, pointLight } from '../core/lights';
 import controls from '../core/controls';
 
 function init() {
+  // Just load the data and call the main func.
+  csv('../../data/corr.csv').then(ready);
+}
+
+function ready(data) {
   controls.rotateSpeed = 4.0;
   camera.position.set(0, 0, 10);
   controls.update();
@@ -22,6 +28,7 @@ function init() {
   const geo = new IcosahedronBufferGeometry(1, 1);
   const mat = new MeshStandardMaterial({ color: '#777', flatShading: true });
   const mesh = new Mesh(geo, mat);
+
   scene.add(mesh);
 }
 
