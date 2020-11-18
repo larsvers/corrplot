65384fb19bf4becdc681cbc856eaac51e078c404
larsvers <larsverspohl@gmail.com>
2020-11-18 22:24:26 +0000

    Determine buttons

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import babel from 'rollup-plugin-babel';
+import babel from '@rollup/plugin-babel';
+import scss from 'rollup-plugin-scss';
 
 export default {
   input: 'src/index.js',
@@ -8,5 +9,20 @@ export default {
     file: 'dist/bundle.js',
     format: 'umd',
   },
-  plugins: [resolve(), commonjs(), babel({ exclude: 'node_modules/**' })],
+  plugins: [
+    resolve(),
+    commonjs(),
+    babel({
+      babelHelpers: 'bundled', // best option when building an app
+      exclude: 'node_modules/**',
+    }),
+    scss({
+      watch: 'css',
+      output: 'dist/bundle.css',
+    }),
+  ],
+  onwarn(warning, warn) {
+    if (warning.code === 'CIRCULAR_DEPENDENCY') return;
+    warn(warning);
+  },
 };
--- src/interact/listener.js
+++ src/interact/listener.js
@@ -1,17 +1,100 @@
 /* eslint-disable import/no-cycle */
-import gsap from 'gsap/src/index';
-import camera from '../core/camera';
-import controls from '../core/controls';
 import {
   highlightCells,
   fadeMeshes,
   toggleGrid,
   tiltDown,
   tiltUp,
-  rotateSprites,
   focusAll,
+  focusQuality,
 } from './handler';
 import { lowlightGrid } from './highlight';
+
+function addFullGrid(grid, rowLabels, colLabels, discs) {
+  document.querySelector('#full-grid').addEventListener('click', function () {
+    focusAll();
+    toggleGrid('show', grid, 'fullGrid');
+    fadeMeshes('show', colLabels, d => d);
+    fadeMeshes('show', rowLabels, d => d);
+    fadeMeshes('show', discs, d => d, 0.01);
+  });
+}
+
+function showQuality(grid, discs, colLabels, rowLabels) {
+  document
+    .querySelector('#show-quality')
+    .addEventListener('click', function () {
+      focusAll();
+      toggleGrid('hide', grid, 'qualityRow');
+      fadeMeshes(
+        'hide',
+        discs,
+        d => d.userData.row !== 'quality' || d.userData.value === 1,
+        0.01
+      );
+      fadeMeshes('hide', colLabels, d => d.userData.col === 'quality');
+      fadeMeshes('hide', rowLabels, d => d.userData.row !== 'quality');
+      focusQuality(colLabels);
+    });
+}
+
+function removeLowerGrid(grid, discs, colLabels) {
+  document
+    .querySelector('#remove-lower-grid')
+    .addEventListener('click', function () {
+      toggleGrid('hide', grid, 'lowerGrid');
+      fadeMeshes('hide', colLabels, d => d.userData.col === 'quality');
+      fadeMeshes(
+        'hide',
+        discs,
+        d => d.userData.index[1] <= d.userData.index[0],
+        0.01
+      );
+    });
+}
+
+function addLowerGrid(grid, discs, colLabels) {
+  document
+    .querySelector('#add-lower-grid')
+    .addEventListener('click', function () {
+      toggleGrid('show', grid, 'fullGrid');
+      fadeMeshes('show', colLabels, d => d.userData.col === 'quality');
+      fadeMeshes(
+        'show',
+        discs,
+        d => d.userData.index[1] <= d.userData.index[0],
+        0.01
+      );
+    });
+}
+
+function removeAutoCorrelation(discs) {
+  document
+    .querySelector('#remove-auto-corr')
+    .addEventListener('click', function () {
+      fadeMeshes('hide', discs, d => d.userData.value === 1);
+    });
+}
+
+function addAutoCorrelation(discs) {
+  document
+    .querySelector('#add-auto-corr')
+    .addEventListener('click', function () {
+      fadeMeshes('show', discs, d => d.userData.value === 1);
+    });
+}
+
+function tiltGrid(labels) {
+  document
+    .querySelector('#tilt-grid')
+    .addEventListener('click', () => tiltDown(labels));
+}
+
+function tiltGridBack(labels) {
+  document
+    .querySelector('#tilt-grid-back')
+    .addEventListener('click', () => tiltUp(labels));
+}
 
 // Actions.
 function highlightSelect(layout, grid) {
@@ -31,148 +114,18 @@ function resetGridColour(grid) {
     });
 }
 
-function removeAutoCorrelation(discs) {
-  document
-    .querySelector('#remove-auto-corr')
-    .addEventListener('click', function () {
-      fadeMeshes('hide', discs, d => d.userData.value === 1);
-    });
-}
-
-function removeLowerDiscs(discs) {
-  document
-    .querySelector('#remove-lower-discs')
-    .addEventListener('click', function () {
-      fadeMeshes(
-        'hide',
-        discs,
-        d => d.userData.index[1] <= d.userData.index[0],
-        0.01
-      );
-    });
-}
-
-function removeLowerGrid(grid, colLabels) {
-  document
-    .querySelector('#remove-lower-grid')
-    .addEventListener('click', function () {
-      toggleGrid('hide', grid);
-      // Remove label.
-      fadeMeshes('hide', colLabels, d => d.userData.col === 'quality');
-    });
-}
-
-function addLowerGrid(grid, colLabels) {
-  document
-    .querySelector('#add-lower-grid')
-    .addEventListener('click', function () {
-      toggleGrid('show', grid);
-      // Add label.
-      fadeMeshes('show', colLabels, d => d.userData.col === 'quality');
-    });
-}
-
-function addLowerDiscs(discs) {
-  document
-    .querySelector('#add-lower-discs')
-    .addEventListener('click', function () {
-      fadeMeshes(
-        'show',
-        discs,
-        d => d.userData.index[1] <= d.userData.index[0],
-        0.01
-      );
-    });
-}
-
-function removeAllButQuality(discs, grid, rowLabels) {
-  document
-    .querySelector('#remove-all-but-quality')
-    .addEventListener('click', function () {
-      // Fade out the discs.
-      fadeMeshes('hide', discs, d => d.userData.row !== 'quality', 0.01);
-      // Fade out the labels.
-      fadeMeshes('hide', rowLabels, d => d.userData.row !== 'quality');
-      // Fade out the grid.
-      toggleGrid('hide', grid, 'qualityRow');
-    });
-}
-
-function addAllButQuality(discs, grid, rowLabels) {
-  document
-    .querySelector('#add-all-but-quality')
-    .addEventListener('click', function () {
-      // Fade out the discs.
-      fadeMeshes('show', discs, d => d.userData.row !== 'quality', 0.01);
-      // Fade out the labels.
-      fadeMeshes('show', rowLabels, d => d.userData.row !== 'quality');
-      // Fade out the grid.
-      toggleGrid('show', grid, 'qualityRow');
-    });
-}
-
-function tiltGrid(labels) {
-  document
-    .querySelector('#tilt-grid')
-    .addEventListener('click', () => tiltDown(labels));
-}
-
-function tiltGridBack(labels) {
-  document
-    .querySelector('#tilt-grid-back')
-    .addEventListener('click', () => tiltUp(labels));
-}
-
-function focusOnQuality(labels) {
-  // Camera move/zoom
-  const zoom = { zoom: 1.5 };
-  const to = { x: 60, y: -100, z: 20 };
-  const up = { x: 0, y: 0, z: 1 };
-  const at = { x: 15, y: 0, z: 0 };
-
-  function onUpdate() {
-    camera.updateProjectionMatrix();
-  }
-
-  function move() {
-    gsap
-      .timeline({ onUpdate })
-      .to(camera, zoom, 0)
-      .to(camera.position, to, 0)
-      .to(camera.up, up, 0)
-      .to(controls.target, at, 0);
-
-    rotateSprites(labels, 90);
-  }
-
-  // Add listener.
-  document
-    .querySelector('#move-camera-position')
-    .addEventListener('click', move);
-}
-
-function setCameraBack() {
-  // Add listener.
-  document
-    .querySelector('#move-camera-back')
-    .addEventListener('click', focusAll);
-}
-
 // Main attaching function.
 function addListener(layout, grid, discs, colLabels, rowLabels) {
+  addFullGrid(grid, rowLabels, colLabels, discs);
   highlightSelect(layout, grid);
   resetGridColour(grid);
   removeAutoCorrelation(discs);
-  removeLowerDiscs(discs);
-  removeLowerGrid(grid, colLabels);
-  addLowerGrid(grid, colLabels);
-  addLowerDiscs(discs);
+  addAutoCorrelation(discs);
+  removeLowerGrid(grid, discs, colLabels);
   tiltGrid(colLabels);
   tiltGridBack(colLabels);
-  removeAllButQuality(discs, grid, rowLabels);
-  addAllButQuality(discs, grid, rowLabels);
-  focusOnQuality(colLabels);
-  setCameraBack();
+  addLowerGrid(grid, discs, colLabels);
+  showQuality(grid, discs, colLabels, rowLabels);
 }
 
 export default addListener;
